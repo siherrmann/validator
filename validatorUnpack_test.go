f13c8bf46d2d61d06e7728bfb5bfd7b164e6e3d0
Simon Herrmann <simon.he3@gmail.com>
2025-07-27 23:37:44 +0200

    add more tests

--- validatorUnpack_test.go
+++ validatorUnpack_test.go
@@ -7,11 +7,12 @@ import (
 	"testing"
 
 	"github.com/siherrmann/validator/model"
+	"github.com/stretchr/testify/assert"
 )
 
 type testStruct struct {
 	Name string `json:"name" vld:"equapple"`
-	Age  int    `json:"age" vld:"min18"`
+	Age  int    `json:"age" vld:"min2"`
 }
 
 func newValidator() *Validator {
@@ -21,142 +22,335 @@ func newValidator() *Validator {
 func TestUnmapOrUnmarshalAndValidate(t *testing.T) {
 	v := newValidator()
 	ts := &testStruct{}
-	form := url.Values{}
-	form.Set("name", "apple")
-	form.Set("age", "30")
-	req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
-	req.Form = form
 
-	err := v.UnmapOrUnmarshalAndValidate(req, ts)
-	if err != nil {
-		t.Errorf("UnmapOrUnmarshalAndValidate failed: %v", err)
-	}
+	t.Run("Valid with form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+		req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
+		req.Form = form
+
+		err := v.UnmapOrUnmarshalAndValidate(req, ts)
+		assert.NoError(t, err, "Expected no error on unmap or unmarshal and validate")
+	})
+
+	t.Run("Invalid with form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "banana")
+		form.Set("age", "1")
+		req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
+		req.Form = form
+
+		err := v.UnmapOrUnmarshalAndValidate(req, ts)
+		assert.Error(t, err, "Expected error on unmap or unmarshal and validate")
+		assert.Contains(t, err.Error(), "error validating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Valid with request body", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"apple","age":2}`))
+
+		err := v.UnmapOrUnmarshalAndValidate(req, ts)
+		assert.NoError(t, err, "Expected no error on unmap or unmarshal and validate")
+	})
+
+	t.Run("Invalid with request body", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"banana","age":1}`))
+
+		err := v.UnmapOrUnmarshalAndValidate(req, ts)
+		assert.Error(t, err, "Expected error on unmap or unmarshal and validate")
+		assert.Contains(t, err.Error(), "error validating struct", "Expected error to contain validation error")
+	})
 }
 
 func TestUnmarshalAndValidate(t *testing.T) {
 	v := newValidator()
 	ts := &testStruct{}
-	jsonInput := []byte(`{"name":"apple","age":30}`)
 
-	err := v.UnmarshalAndValidate(jsonInput, ts)
-	if err != nil {
-		t.Errorf("UnmarshalAndValidate failed: %v", err)
-	}
+	t.Run("Valid JSON input", func(t *testing.T) {
+		jsonInput := []byte(`{"name":"apple","age":2}`)
+
+		err := v.UnmarshalAndValidate(jsonInput, ts)
+		assert.NoError(t, err, "Expected no error on unmarshal and validate")
+	})
+
+	t.Run("Invalid JSON values", func(t *testing.T) {
+		jsonInput := []byte(`{"name":"banana","age":1}`)
+
+		err := v.UnmarshalAndValidate(jsonInput, ts)
+		assert.Error(t, err, "Expected error on unmarshal and validate")
+		assert.Contains(t, err.Error(), "error validating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Invalid JSON input", func(t *testing.T) {
+		jsonInput := []byte(`<html><body>Invalid JSON</body></html>`)
+
+		err := v.UnmarshalAndValidate(jsonInput, ts)
+		assert.Error(t, err, "Expected error on unmarshal and validate with invalid JSON")
+		assert.Contains(t, err.Error(), "error unmarshaling json:", "Expected error to contain unmarshaling error")
+	})
+
+	t.Run("Invalid struct type", func(t *testing.T) {
+		ts := testStruct{}
+		jsonInput := []byte(`{"name":"apple","age":2}`)
+
+		err := v.UnmarshalAndValidate(jsonInput, ts)
+		assert.Error(t, err, "Expected error on unmarshal and validate with invalid struct type")
+		assert.Contains(t, err.Error(), "value has to be of kind pointer", "Expected error to contain validation error")
+	})
 }
 
 func TestUnmapAndValidate(t *testing.T) {
 	v := newValidator()
 	ts := &testStruct{}
-	form := url.Values{}
-	form.Set("name", "apple")
-	form.Set("age", "30")
 
-	err := v.UnmapAndValidate(form, ts)
-	if err != nil {
-		t.Errorf("UnmapAndValidate failed: %v", err)
-	}
+	t.Run("Valid form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+
+		err := v.UnmapAndValidate(form, ts)
+		assert.NoError(t, err, "Expected no error on unmap and validate")
+	})
+
+	t.Run("Invalid form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "banana")
+		form.Set("age", "1")
+
+		err := v.UnmapAndValidate(form, ts)
+		assert.Error(t, err, "Expected error on unmap and validate")
+		assert.Contains(t, err.Error(), "error validating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Invalid struct type", func(t *testing.T) {
+		ts := testStruct{}
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+
+		err := v.UnmapAndValidate(form, ts)
+		assert.Error(t, err, "Expected error on unmap and validate")
+		assert.Contains(t, err.Error(), "value has to be of kind pointer", "Expected error to contain validation error")
+	})
 }
-
 func TestUnmapOrUnmarshalValidateAndUpdate(t *testing.T) {
 	v := newValidator()
 	ts := &testStruct{}
-	form := url.Values{}
-	form.Set("name", "apple")
-	form.Set("age", "30")
-	req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
-	req.Form = form
 
-	err := v.UnmapOrUnmarshalValidateAndUpdate(req, ts)
-	if err != nil {
-		t.Errorf("UnmapOrUnmarshalValidateAndUpdate failed: %v", err)
-	}
+	t.Run("Valid with form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+		req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
+		req.Form = form
+
+		err := v.UnmapOrUnmarshalValidateAndUpdate(req, ts)
+		assert.NoError(t, err, "Expected no error on unmap or unmarshal validate and update")
+	})
+
+	t.Run("Invalid with form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "banana")
+		form.Set("age", "1")
+		req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
+		req.Form = form
+
+		err := v.UnmapOrUnmarshalValidateAndUpdate(req, ts)
+		assert.Error(t, err, "Expected error on unmap or unmarshal validate and update")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Valid with request body", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"apple","age":2}`))
+
+		err := v.UnmapOrUnmarshalValidateAndUpdate(req, ts)
+		assert.NoError(t, err, "Expected no error on unmap or unmarshal validate and update")
+	})
+
+	t.Run("Invalid with request body", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"banana","age":1}`))
+
+		err := v.UnmapOrUnmarshalValidateAndUpdate(req, ts)
+		assert.Error(t, err, "Expected error on unmap or unmarshal validate and update")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
 }
 
 func TestUnmarshalValidateAndUpdate(t *testing.T) {
 	v := newValidator()
 	ts := &testStruct{}
-	jsonInput := []byte(`{"name":"apple","age":30}`)
 
-	err := v.UnmarshalValidateAndUpdate(jsonInput, ts)
-	if err != nil {
-		t.Errorf("UnmarshalValidateAndUpdate failed: %v", err)
-	}
+	t.Run("Valid JSON input", func(t *testing.T) {
+		jsonInput := []byte(`{"name":"apple","age":2}`)
+
+		err := v.UnmarshalValidateAndUpdate(jsonInput, ts)
+		assert.NoError(t, err, "Expected no error on unmarshal validate and update")
+	})
+
+	t.Run("Invalid JSON values", func(t *testing.T) {
+		jsonInput := []byte(`{"name":"banana","age":1}`)
+
+		err := v.UnmarshalValidateAndUpdate(jsonInput, ts)
+		assert.Error(t, err, "Expected error on unmarshal validate and update")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Invalid JSON input", func(t *testing.T) {
+		jsonInput := []byte(`<html><body>Invalid JSON</body></html>`)
+
+		err := v.UnmarshalValidateAndUpdate(jsonInput, ts)
+		assert.Error(t, err, "Expected error on unmarshal validate and update with invalid JSON")
+		assert.Contains(t, err.Error(), "error unmarshaling request body", "Expected error to contain unmarshaling error")
+	})
+
+	t.Run("Invalid struct type", func(t *testing.T) {
+		ts := testStruct{}
+		jsonInput := []byte(`{"name":"apple","age":2}`)
+
+		err := v.UnmarshalValidateAndUpdate(jsonInput, ts)
+		assert.Error(t, err, "Expected error on unmarshal validate and update with invalid struct type")
+		assert.Contains(t, err.Error(), "value has to be of kind pointer", "Expected error to contain validation error")
+	})
 }
 
 func TestUnmapValidateAndUpdate(t *testing.T) {
 	v := newValidator()
 	ts := &testStruct{}
-	form := url.Values{}
-	form.Set("name", "apple")
-	form.Set("age", "30")
 
-	err := v.UnmapValidateAndUpdate(form, ts)
-	if err != nil {
-		t.Errorf("UnmapValidateAndUpdate failed: %v", err)
-	}
+	t.Run("Valid form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+
+		err := v.UnmapValidateAndUpdate(form, ts)
+		assert.NoError(t, err, "Expected no error on unmap validate and update")
+	})
+
+	t.Run("Invalid form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "banana")
+		form.Set("age", "1")
+
+		err := v.UnmapValidateAndUpdate(form, ts)
+		assert.Error(t, err, "Expected error on unmap validate and update")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Invalid struct type", func(t *testing.T) {
+		ts := testStruct{}
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+
+		err := v.UnmapValidateAndUpdate(form, ts)
+		assert.Error(t, err, "Expected error on unmap validate and update with invalid struct type")
+		assert.Contains(t, err.Error(), "value has to be of kind pointer", "Expected error to contain validation error")
+	})
 }
 
 func TestUnmapOrUnmarshalValidateAndUpdateWithValidation(t *testing.T) {
 	v := newValidator()
 	validations := []model.Validation{
-		{
-			Key:         "name",
-			Type:        model.String,
-			Requirement: "equapple",
-		},
-		{
-			Key:         "age",
-			Type:        model.Int,
-			Requirement: "min18",
-		},
+		{Key: "name", Requirement: "equapple"},
+		{Key: "age", Requirement: "min2"},
 	}
+	mapToUpdate := &model.JsonMap{}
 
-	t.Run("With form values", func(t *testing.T) {
-		mapOut := &model.JsonMap{}
+	t.Run("Valid with form values", func(t *testing.T) {
 		form := url.Values{}
 		form.Set("name", "apple")
-		form.Set("age", "30")
+		form.Set("age", "2")
 		req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
 		req.Form = form
 
-		err := v.UnmapOrUnmarshalValidateAndUpdateWithValidation(req, mapOut, validations)
-		if err != nil {
-			t.Errorf("UnmapOrUnmarshalValidateAndUpdateWithValidation failed: %v", err)
-		}
+		err := v.UnmapOrUnmarshalValidateAndUpdateWithValidation(req, mapToUpdate, validations)
+		assert.NoError(t, err, "Expected no error on unmap or unmarshal validate and update with validation")
 	})
 
-	t.Run("With request body", func(t *testing.T) {
-		mapOut := &model.JsonMap{}
-		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"apple","age":30}`))
-		err := v.UnmapOrUnmarshalValidateAndUpdateWithValidation(req, mapOut, validations)
-		if err != nil {
-			t.Errorf("UnmapOrUnmarshalValidateAndUpdateWithValidation failed: %v", err)
-		}
+	t.Run("Invalid with form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "banana")
+		form.Set("age", "1")
+		req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(nil))
+		req.Form = form
+
+		err := v.UnmapOrUnmarshalValidateAndUpdateWithValidation(req, mapToUpdate, validations)
+		assert.Error(t, err, "Expected error on unmap or unmarshal validate and update with validation")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Valid with request body", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"apple","age":2}`))
+
+		err := v.UnmapOrUnmarshalValidateAndUpdateWithValidation(req, mapToUpdate, validations)
+		assert.NoError(t, err, "Expected no error on unmap or unmarshal validate and update with validation")
+	})
+
+	t.Run("Invalid with request body", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "/", bytes.NewBufferString(`{"name":"banana","age":1}`))
+
+		err := v.UnmapOrUnmarshalValidateAndUpdateWithValidation(req, mapToUpdate, validations)
+		assert.Error(t, err, "Expected error on unmap or unmarshal validate and update with validation")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
 	})
 }
 
 func TestUnmarshalValidateAndUpdateWithValidation(t *testing.T) {
 	v := newValidator()
-	mapOut := &model.JsonMap{}
-	jsonInput := []byte(`{"name":"apple","age":30}`)
-	validations := []model.Validation{}
-
-	err := v.UnmarshalValidateAndUpdateWithValidation(jsonInput, mapOut, validations)
-	if err != nil {
-		t.Errorf("UnmarshalValidateAndUpdateWithValidation failed: %v", err)
+	validations := []model.Validation{
+		{Key: "name", Requirement: "equapple"},
+		{Key: "age", Requirement: "min2"},
 	}
+	mapToUpdate := &model.JsonMap{}
+
+	t.Run("Valid JSON input", func(t *testing.T) {
+		jsonInput := []byte(`{"name":"apple","age":2}`)
+
+		err := v.UnmarshalValidateAndUpdateWithValidation(jsonInput, mapToUpdate, validations)
+		assert.NoError(t, err, "Expected no error on unmarshal validate and update with validation")
+	})
+
+	t.Run("Invalid JSON values", func(t *testing.T) {
+		jsonInput := []byte(`{"name":"banana","age":1}`)
+
+		err := v.UnmarshalValidateAndUpdateWithValidation(jsonInput, mapToUpdate, validations)
+		assert.Error(t, err, "Expected error on unmarshal validate and update with validation")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
+
+	t.Run("Invalid JSON input", func(t *testing.T) {
+		jsonInput := []byte(`<html><body>Invalid JSON</body></html>`)
+
+		err := v.UnmarshalValidateAndUpdateWithValidation(jsonInput, mapToUpdate, validations)
+		assert.Error(t, err, "Expected error on unmarshal validate and update with validation with invalid JSON")
+		assert.Contains(t, err.Error(), "error unmarshaling", "Expected error to contain unmarshaling error")
+	})
 }
 
 func TestUnmapValidateAndUpdateWithValidation(t *testing.T) {
 	v := newValidator()
-	mapOut := &model.JsonMap{}
-	form := url.Values{}
-	form.Set("name", "apple")
-	form.Set("age", "30")
-	validations := []model.Validation{}
-
-	err := v.UnmapValidateAndUpdateWithValidation(form, mapOut, validations)
-	if err != nil {
-		t.Errorf("UnmapValidateAndUpdateWithValidation failed: %v", err)
+	validations := []model.Validation{
+		{Key: "name", Requirement: "equapple"},
+		{Key: "age", Requirement: "min2"},
 	}
+	mapToUpdate := &model.JsonMap{}
+
+	t.Run("Valid form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "apple")
+		form.Set("age", "2")
+
+		err := v.UnmapValidateAndUpdateWithValidation(form, mapToUpdate, validations)
+		assert.NoError(t, err, "Expected no error on unmap validate and update with validation")
+	})
+
+	t.Run("Invalid form values", func(t *testing.T) {
+		form := url.Values{}
+		form.Set("name", "banana")
+		form.Set("age", "1")
+
+		err := v.UnmapValidateAndUpdateWithValidation(form, mapToUpdate, validations)
+		assert.Error(t, err, "Expected error on unmap validate and update with validation")
+		assert.Contains(t, err.Error(), "error updating struct", "Expected error to contain validation error")
+	})
 }
